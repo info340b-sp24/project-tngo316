cb147bf2cedc95c59f292a057b43e1de57a81b1d
Yining Wang <yining@Yinings-MacBook-Air.local>
2024-05-16 21:41:12 -0700

    changes to the app.js

--- nbaforum/src/components/App.js
+++ nbaforum/src/components/App.js
@@ -1,2 +1,17 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { Header } from './Header';
+import PlayerList from './playerlist';
+import { Footer } from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header message="NBA Statistics" />
+      <PlayerList />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
--- nbaforum/src/components/playerlist.js
+++ nbaforum/src/components/playerlist.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PlayerCard from './components/PlayerCard';
+import PlayerCard from './PlayerCard';
 
 const players = [
     // Atlanta Hawks
