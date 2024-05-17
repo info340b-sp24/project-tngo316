5d1bcc21455f3bb30e13c697e45634cad2fe21db
Mark <mxpm02@uw.edu>
2024-05-16 20:48:45 -0700

    added footer and header components

---
+++ nbaforum/src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export function Footer(props){
+    return(
+        <div>
+            <p>University of Washington INFO340</p>
+            <p>Spring 24, Group 6</p>
+        </div>
+    );
+}
